3bb684aee4e66ca9da6bde5126d9fe721f75ee6c
Lakshya Sharma <106482122+lakshya2207@users.noreply.github.com>
2023-09-06 20:43:41 +0530

    host it

---
+++ index.js
@@ -0,0 +1,23 @@
+let a=34;
+console.log(a);
+
+let b="Lakshya Sharma";
+console.log(b);
+function hello(){
+    console.log("function is wroking")
+}
+func: function print(x){
+    console.log("you entered : ",x)
+}
+function myFunction() {
+    var x = document.getElementById("menu");
+    if (x.style.display == "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+ }
+// document.addEventListener("click",function click(){
+//     console.log("clicked successfully")
+//     let con=confirm("Is the profile good ?")
+// })
