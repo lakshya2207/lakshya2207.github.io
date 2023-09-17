9b7533d07b10d6cb5d56a0e5d5c6685e1536af0d
Lakshya Sharma <106482122+lakshya2207@users.noreply.github.com>
2023-09-17 14:29:11 +0530

    hamburger & design enhancement

--- index.js
+++ index.js
@@ -1,23 +1,14 @@
-let a=34;
-console.log(a);
 
-let b="Lakshya Sharma";
-console.log(b);
-function hello(){
-    console.log("function is wroking")
-}
-func: function print(x){
-    console.log("you entered : ",x)
-}
 function myFunction() {
-    var x = document.getElementById("menu");
-    if (x.style.display == "none") {
+    var x = document.getElementById("hammenu");
+    if (x.style.display === "none") {
       x.style.display = "block";
-    } else {
+
+    }
+    else {
       x.style.display = "none";
     }
  }
-// document.addEventListener("click",function click(){
-//     console.log("clicked successfully")
-//     let con=confirm("Is the profile good ?")
-// })
+function closemenu() {
+  document.getElementById("hammenu").style.display="none";
+}
